197703394ce1a72800e0579822395a538f8d4f0d
Arester <temultik@gmail.com>
2020-06-05 14:32:13 +0300

    add(code): realize create user point and random point

--- geo_bot.py
+++ geo_bot.py
@@ -3,18 +3,10 @@ from random import uniform
 import telebot
 import jsonpickle
 import json
-# import os
 
-# token = os.getenv("token")
 bot = telebot.TeleBot('1171904194:AAGtjGvd_oKWFUOUa_DNg0o3UdrN7zR_sZk')
 
-MINSK = (53.902221, 27.561924)
-PRILUKI = (53.797048, 27.450923)
-
-place = []
-links = []
-
-# 1171904194:AAGtjGvd_oKWFUOUa_DNg0o3UdrN7zR_sZk
+user_location = (53.902221, 27.561924)  # положение user по умолчанию (Центр Минска)
 
 
 @bot.message_handler(commands=['start'])
@@ -30,136 +22,16 @@ def get_locate(message):
 @bot.message_handler(content_types=['text'])
 def get_data(message):
     if message.text == 'Любая точка Беларуси':
-        lon = round(uniform(51.262, 56.172), 4)
-        lat = round(uniform(23.178, 32.777), 4)
-        lon, lat = str(lon), str(lat)
-        coord = '{},{}'.format(lon, lat)
-        geolocator = Nominatim(user_agent='geobot')
-        location = geolocator.reverse(coord) # получаем место на карте по координатам
-        r = location.raw # получаем json
-        adr = r.get('address')  # из json берем адрес
-        cc = adr.get('country_code') # из адреса берем код страны
-        if cc == 'by':
-            place.clear()
-            loc = location.address  # строка с человекочитаемым адресом
-            place.append(coord) # переписываем значение координат места
-            place.append(loc)
-            dist = distance.distance(MINSK, coord)  # вычисление расстояния между Минском и адресом
-            dist = str(dist)
-            dist = dist.split(' ')
-            dist = round(float(dist[0]), 2)  # округленное значение расстояния
-            dist = 'Растояние от Октябрьской площади Минска до этого места: {} km.\nЧто бы узнать расстояние от ' \
-                    'твоего местонахождения до {} пришли боту свою геолокацию или проложи свой маршрут с ' \
-                    'использованием одного из сервисов'.format(dist, loc.split(',')[0])
-            links.clear()
-            links.append('Это место на картах Google":\nhttps://www.google.com/maps/place/{}'.format(coord))
-            links.append('Это место на картах Yandex:\nhttps://yandex.com/maps/?text={}'.format(coord))
-            links.append('Это место на картах архитектурного наследия Беларуси:\nhttps://orda.of.by/.map/?{}'.format(coord))
-            links.append('Это место на картах MapsMe:\nmaps.google.com/{}'.format(coord))
-            keyboard = telebot.types.InlineKeyboardMarkup()
-            key_ya = telebot.types.InlineKeyboardButton(text='Yandex', callback_data='ya')
-            key_gg = telebot.types.InlineKeyboardButton(text='Google', callback_data='gg')
-            key_globus = telebot.types.InlineKeyboardButton(text='карты Globus.tut.by', callback_data='globus')
-            key_maps = telebot.types.InlineKeyboardButton(text='приложение Maps.me', callback_data='maps')
-            keyboard.add(key_ya, key_gg)
-            keyboard.add(key_globus, key_maps)
-            bot.send_message(message.chat.id, "Почему бы сегодня не отправится:\n{}".format(loc))
-            bot.send_message(message.chat.id, dist, reply_markup=keyboard)
-        else:
-            print('None BY')
-            get_data(message)
-    if message.text == 'Точка на определенном расстоянии от тебя':
-        bot.send_message(message.chat.id, 'Пришли боту свою геолокацию а затем введи количество километров, которое '
-                                          'ты готов преодолеть сегодня. Точка будет на этом расстоянии +/- 20%')
-    elif message.text != 'Точка на определенном расстоянии от тебя' or 'Любая точка Беларуси':
-        x_json = jsonpickle.encode(message)
-        x_list = x_json.split(':')
-        x_rawtext = x_list[82]
-        x_text = x_rawtext.replace('"', '')
-        x_text = x_text.rstrip('}')
-        x_text = x_text.replace(' ', '')  # содержание сообщения
-        if x_text.isnumeric():  # проверка сообщения на число
-            rang = int(x_text)
-            coord, rang_rng = loc_coord(rang)  # вернулся диапазон расстояний 20%
-            geolocator = Nominatim(user_agent='geobot')
-            location = geolocator.reverse(coord)  # получаем место на карте по координатам
-            dist = distance.distance(PRILUKI, coord)
-            dist = str(dist)
-            dist = dist.split(' ')
-            dist = round(float(dist[0]))  # из введенного пользователя значения получаем километраж
-            if dist in rang_rng:
-                adr = location.address
-                place.append(adr)
-                dist = str(dist)
-                dist = dist.split(' ')
-                dist = round(float(dist[0]), 2)  # округленное значение расстояния
-                dist = 'Растояние от Прилук до {} этого места: {} km'.format(adr, dist)
-                bot.send_message(message.chat.id, dist)
-            else:
-                print('another distance')
-                # parc(message, x_text)
-        else:
-            bot.send_message(message.chat.id, 'Введите число или нажмите одну из кнопок')
+        anypoint = AnyPoint()
+        print(anypoint)
 
-        # parc(message, x_text)
-
-# def parc(message, x_text):
-    # x_json = jsonpickle.encode(message)
-    # x_list = x_json.split(':')
-    # x_rawtext = x_list[82]
-    # x_text = x_rawtext.replace('"', '')
-    # x_text = x_text.rstrip('}')
-    # x_text = x_text.replace(' ', '') # содержание сообщения
-
-
-def loc_coord(rang):
-    global coord
-    rang_prc = rang/5 #20 процентов от желаемой дистанции
-    rang_min = int(rang-rang_prc)
-    rang_max = int(rang+rang_prc)
-    rang_rng = [i for i in range(rang_min, rang_max)] # список расстояний от минимального до макисмального
-    if rang >= 350:
-        lon = round(uniform(54.551550, 56.172), 4)
-        lat = round(uniform(30.224864, 32.777), 4)
-        lon, lat = str(lon), str(lat)
-        lon, lat = str(lon), str(lat)
-        coord = '{},{}'.format(lon, lat)
-    if rang < 350 > 250:
-        lon = round(uniform(54.551549, 55.198531), 4)
-        lat = round(uniform(29.240142, 30.224863), 4)
-        lon, lat = str(lon), str(lat)
-        coord = '{},{}'.format(lon, lat)
-    if rang < 250 > 100:
-        lon = round(uniform(54.318075, 54.551548), 4)
-        lat = round(uniform(28.721071, 29.240141), 4)
-        lon, lat = str(lon), str(lat)
-        coord = '{},{}'.format(lon, lat)
-    if rang < 100:
-        lon = round(uniform(53.527442, 54.318074), 4)
-        lat = round(uniform(26.388708, 28.721070), 4)
-        lon, lat = str(lon), str(lat)
-        coord = '{},{}'.format(lon, lat)
-    return coord, rang_rng
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback_worker(call):
-    global links
-    if call.data == "ya":
-        bot.send_message(call.message.chat.id, links[1])
-    if call.data == "gg":
-        bot.send_message(call.message.chat.id, links[0])
-    if call.data == "globus":
-        bot.send_message(call.message.chat.id, links[2])
-    if call.data == "maps":
-        bot.send_message(call.message.chat.id, links[3])
-
-
-@bot.message_handler(content_types=['location'])
+@bot.message_handler(content_types=['location']) # получает локацию и переписывает user_location
 def calc_distance(message):
-    global place
+    global user_location
     x = jsonpickle.encode(message)
     xs = x.split(',')
     xsr = xs[78:80]
+    print(xsr)
     lat = []
     lon = []
     for i in xsr[0]:
@@ -172,16 +44,185 @@ def calc_distance(message):
     loc_float = []
     for i in loc_list:
         loc_float.append(float(i))
-    y = place[0].split(',')
-    place_float = []
-    # place_float.clear()
-    for i in y:
-        place_float.append(float(i))
-    yourdist = str(distance.distance(loc_float, place_float))
-    yourdist = yourdist.split(' ')
-    yourdist = round(float(yourdist[0]), 2)
-    your_place = place[1]
-    bot.send_message(message.chat.id, 'Расстояние от тебя до {}: {} km'.format(your_place.split(',')[0], yourdist))
+    user_location = UserPoint(loc_float[0])
+    print(user_location.coord())
+
+
+class UserPoint:
+    def __init__(self, lon=53.902221, lat=27.561924):  # по умолчанию минск
+        self.lon = lon
+        self.lat = lat
+
+    def coord(self):  # формат координат строка
+        lon, lat = str(self.lon), str(self.lat)
+        coord = '{},{}'.format(lon, lat)
+        return coord
+
+class AnyPoint(UserPoint):  # случайная точка на карте. наследование дает возможность использовать здесь coord
+    def __init__(self):
+        x = True
+        while x == True:
+            self.lon = round(uniform(51.262, 56.172), 4)
+            self.lat = round(uniform(23.178, 32.777), 4)
+            coord = [self.lon, self.lat]
+            geolocator = Nominatim(user_agent='geobot')
+            location = geolocator.reverse(coord) # получаем место на карте по координатам
+            r = location.raw # получаем json
+            adr = r.get('address')  # из json берем адрес
+            cc = adr.get('country_code') # из адреса берем код страны
+            if cc == 'by':
+                print(coord)
+                print(adr)
+                x = False
+            else:
+                print('Non BY')
+                print(adr)
+#
+# class Distation:
+#     def calc_dis(self, user, place):
+#         self.user = MSQ.coord()
+#         self.place = ANY.coord()
+#         print('пользователь {} + место {}'.format(self.user, self.place))
+#         return str('пользователь {} + место {}'.format(self.user, self.place))
+#
+#
+# MSQ = UserPoint()
+# PRK = UserPoint(message1[0], message1[1])
+# print(f'MSQ: {MSQ.coord()}')
+# print(f'PRK: {PRK.coord()}')
+#
+# ANY = AnyPoint()
+# print(f'ANY coord: {ANY.coord()}')
+#
+# DIST = Distation()
+# print(f'Distance: {DIST.calc_dis(MSQ, ANY)}')
+#
+#
+# @bot.message_handler(content_types=['text'])
+# def get_data(message):
+#     if message.text == 'Любая точка Беларуси':
+#         lon = round(uniform(51.262, 56.172), 4)
+#         lat = round(uniform(23.178, 32.777), 4)
+#         lon, lat = str(lon), str(lat)
+#         coord = '{},{}'.format(lon, lat)
+#         geolocator = Nominatim(user_agent='geobot')
+#         location = geolocator.reverse(coord) # получаем место на карте по координатам
+#         r = location.raw # получаем json
+#         adr = r.get('address')  # из json берем адрес
+#         cc = adr.get('country_code') # из адреса берем код страны
+#         if cc == 'by':
+#             place.clear()
+#             loc = location.address  # строка с человекочитаемым адресом
+#             place.append(coord) # переписываем значение координат места
+#             place.append(loc)
+#             dist = distance.distance(MINSK, coord)  # вычисление расстояния между Минском и адресом
+#             dist = str(dist)
+#             dist = dist.split(' ')
+#             dist = round(float(dist[0]), 2)  # округленное значение расстояния
+#             dist = 'Растояние от Октябрьской площади Минска до этого места: {} km.\nЧто бы узнать расстояние от ' \
+#                     'твоего местонахождения до {} пришли боту свою геолокацию или проложи свой маршрут с ' \
+#                     'использованием одного из сервисов'.format(dist, loc.split(',')[0])
+#             links.clear()
+#             links.append('Это место на картах Google":\nhttps://www.google.com/maps/place/{}'.format(coord))
+#             links.append('Это место на картах Yandex:\nhttps://yandex.com/maps/?text={}'.format(coord))
+#             links.append('Это место на картах архитектурного наследия Беларуси:\nhttps://orda.of.by/.map/?{}'.format(coord))
+#             links.append('Это место на картах MapsMe:\nmaps.google.com/{}'.format(coord))
+#             keyboard = telebot.types.InlineKeyboardMarkup()
+#             key_ya = telebot.types.InlineKeyboardButton(text='Yandex', callback_data='ya')
+#             key_gg = telebot.types.InlineKeyboardButton(text='Google', callback_data='gg')
+#             key_globus = telebot.types.InlineKeyboardButton(text='карты Globus.tut.by', callback_data='globus')
+#             key_maps = telebot.types.InlineKeyboardButton(text='приложение Maps.me', callback_data='maps')
+#             keyboard.add(key_ya, key_gg)
+#             keyboard.add(key_globus, key_maps)
+#             bot.send_message(message.chat.id, "Почему бы сегодня не отправится:\n{}".format(loc))
+#             bot.send_message(message.chat.id, dist, reply_markup=keyboard)
+#         else:
+#             print('None BY')
+#             get_data(message)
+#     if message.text == 'Точка на определенном расстоянии от тебя':
+#         bot.send_message(message.chat.id, 'Пришли боту свою геолокацию а затем введи количество километров, которое '
+#                                           'ты готов преодолеть сегодня. Точка будет на этом расстоянии +/- 20%')
+#     elif message.text != 'Точка на определенном расстоянии от тебя' or 'Любая точка Беларуси':
+#         x_json = jsonpickle.encode(message)
+#         x_list = x_json.split(':')
+#         x_rawtext = x_list[82]
+#         x_text = x_rawtext.replace('"', '')
+#         x_text = x_text.rstrip('}')
+#         x_text = x_text.replace(' ', '')  # содержание сообщения
+#         if x_text.isnumeric():  # проверка сообщения на число
+#             rang = int(x_text)
+#             coord, rang_rng = loc_coord(rang)  # вернулся диапазон расстояний 20%
+#             geolocator = Nominatim(user_agent='geobot')
+#             location = geolocator.reverse(coord)  # получаем место на карте по координатам
+#             dist = distance.distance(PRILUKI, coord)
+#             dist = str(dist)
+#             dist = dist.split(' ')
+#             dist = round(float(dist[0]))  # из введенного пользователя значения получаем километраж
+#             if dist in rang_rng:
+#                 adr = location.address
+#                 place.append(adr)
+#                 dist = str(dist)
+#                 dist = dist.split(' ')
+#                 dist = round(float(dist[0]), 2)  # округленное значение расстояния
+#                 dist = 'Растояние от Прилук до {} этого места: {} km'.format(adr, dist)
+#                 bot.send_message(message.chat.id, dist)
+#             else:
+#                 print('another distance')
+#                 # parc(message, x_text)
+#         else:
+#             bot.send_message(message.chat.id, 'Введите число или нажмите одну из кнопок')
+#
+#         # parc(message, x_text)
+#
+# # def parc(message, x_text):
+#     # x_json = jsonpickle.encode(message)
+#     # x_list = x_json.split(':')
+#     # x_rawtext = x_list[82]
+#     # x_text = x_rawtext.replace('"', '')
+#     # x_text = x_text.rstrip('}')
+#     # x_text = x_text.replace(' ', '') # содержание сообщения
+#
+#
+# def loc_coord(rang):
+#     global coord
+#     rang_prc = rang/5 #20 процентов от желаемой дистанции
+#     rang_min = int(rang-rang_prc)
+#     rang_max = int(rang+rang_prc)
+#     rang_rng = [i for i in range(rang_min, rang_max)] # список расстояний от минимального до макисмального
+#     if rang >= 350:
+#         lon = round(uniform(54.551550, 56.172), 4)
+#         lat = round(uniform(30.224864, 32.777), 4)
+#         lon, lat = str(lon), str(lat)
+#         lon, lat = str(lon), str(lat)
+#         coord = '{},{}'.format(lon, lat)
+#     if rang < 350 > 250:
+#         lon = round(uniform(54.551549, 55.198531), 4)
+#         lat = round(uniform(29.240142, 30.224863), 4)
+#         lon, lat = str(lon), str(lat)
+#         coord = '{},{}'.format(lon, lat)
+#     if rang < 250 > 100:
+#         lon = round(uniform(54.318075, 54.551548), 4)
+#         lat = round(uniform(28.721071, 29.240141), 4)
+#         lon, lat = str(lon), str(lat)
+#         coord = '{},{}'.format(lon, lat)
+#     if rang < 100:
+#         lon = round(uniform(53.527442, 54.318074), 4)
+#         lat = round(uniform(26.388708, 28.721070), 4)
+#         lon, lat = str(lon), str(lat)
+#         coord = '{},{}'.format(lon, lat)
+#     return coord, rang_rng
+#
+# @bot.callback_query_handler(func=lambda call: True)
+# def callback_worker(call):
+#     global links
+#     if call.data == "ya":
+#         bot.send_message(call.message.chat.id, links[1])
+#     if call.data == "gg":
+#         bot.send_message(call.message.chat.id, links[0])
+#     if call.data == "globus":
+#         bot.send_message(call.message.chat.id, links[2])
+#     if call.data == "maps":
+#         bot.send_message(call.message.chat.id, links[3])
 
 
 bot.polling()
